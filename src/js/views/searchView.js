86054058357902e93cda19a283bc2a50e2114ddd
d <g>
2018-12-03 15:49:38 +0100

    Title lengts

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,7 +8,21 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResultList.innerHTML = '';
-}
+};
+
+export const limitRecipeTitie = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
 const renderRecipie = recipe => {
     const markup = `
     <li>
@@ -17,7 +31,7 @@ const renderRecipie = recipe => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitie(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
