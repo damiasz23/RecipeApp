0f050e42f6cddadf450a2fe249b7611b398c758b
d <g>
2018-12-03 10:46:58 +0100

    setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './test';
-
-console.log(`I import ${x} from another module1!`);
+const num = 23;
+console.log(`I import ${x} from another module1! Varible num is: ${num}`);
