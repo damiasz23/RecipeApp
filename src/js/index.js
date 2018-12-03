327542649fe9507d3d9ee948c0a258c0b9450b9a
d <g>
2018-12-03 12:31:06 +0100

    first API query

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
+import axios from 'axios';
+
+
+async function getRes(query) {
+    const key = 'd9696833f4ca33270a750b41ef7e770a';
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    console.log(res);
+}
+getRes('pizza');
 
 
